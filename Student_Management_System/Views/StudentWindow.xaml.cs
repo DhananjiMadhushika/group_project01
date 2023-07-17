47f6561d83dd714ceced134636f1a039fd7c128e
Dhananji Madhushika <dhananjimadushika72@gmail.com>
2023-07-17 12:33:02 +0530

    first comment

---
+++ Student_Management_System/Views/StudentWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Student_Management_System.ViewModels;
+using System.Windows;
+
+namespace Student_Management_System.Views
+{
+    /// <summary>
+    /// Interaction logic for StudentWindow.xaml
+    /// </summary>
+    public partial class StudentWindow : Window
+    {
+        public StudentWindow()
+        {
+            InitializeComponent();
+            var vm = new StudentWindowViewModel();
+            DataContext= vm;
+            vm.CloseAction = () => Close();
+            
+        }
+        
+        
+       
+    }
+}
